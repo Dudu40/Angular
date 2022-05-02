c1b69426cc6b937dee4083386597c742176d0a56
ducasseque <ducasseque@eisti.eu>
2022-05-02 19:50:52 +0200

    add front

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'entrainement'`, () => {
+  it(`should have as title 'cookiesApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('entrainement');
+    expect(app.title).toEqual('cookiesApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('entrainement app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('cookiesApp app is running!');
   });
 });
